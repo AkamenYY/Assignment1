5462319eaceb4f6c1e169eff72b7ab10f644beb0
AkamenYY <klinokdemonov468@gmail.com>
2025-02-14 22:52:32 +0500

    commit 2 i added class of organazation and player

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,61 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Esportsman {
+    private String name;
+    private String role;
+
+    public Esportsman() {}
+
+    public Esportsman(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
     }
 }
+
+class Organization {
+    private String orgName;
+    private String region;
+
+    public Organization(String orgName, String region) {
+        this.orgName = orgName;
+        this.region = region;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+}
+
+
